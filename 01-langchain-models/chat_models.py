d57ae3cb2473ed729a07652f67b172db43734a02
Mohit Kumar <37mohitkumar.mk@gmail.com>
2026-05-13 22:23:53 +0530

    Prompt basics

--- 01-langchain-models/chat_models.py
+++ 01-langchain-models/chat_models.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 first_chatmodel = ChatOpenAI(model="gpt-4")
 
-result = first_chatmodel.invoke("Who is Gojo Satoru?")
+result = first_chatmodel.invoke("Who is Goku in Dragon Ball Z?")
 print(result.content)
